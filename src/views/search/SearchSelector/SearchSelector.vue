0fed8bdb26e3e203e051f570d414221b1c607af9
xiumubai <1547702880@qq.com>
2022-11-18 14:57:23 +0800

    feat: del sleect more

--- src/views/search/SearchSelector/SearchSelector.vue
+++ src/views/search/SearchSelector/SearchSelector.vue
@@ -15,7 +15,7 @@
 				</ul>
 			</div>
 			<div class="ext">
-				<a href="javascript:void(0);" class="sui-btn">多选</a>
+				<!-- <a href="javascript:void(0);" class="sui-btn">多选</a> -->
 				<a href="javascript:void(0);">更多</a>
 			</div>
 		</div>
@@ -140,6 +140,7 @@ const searchAttr = (attr: string) => {
 					a {
 						text-decoration: none;
 						color: #666;
+						cursor: pointer;
 					}
 				}
 			}
